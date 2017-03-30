44cb99e5617991ab524aada7704b2e24614e25b5
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-03-30 15:17:08 +0200

    Recognized some non magnolia core classes as cache key entries.

--- src/main/java/nl/vpro/magnolia/jsr107/NodeAwareCacheKeyGenerator.java
+++ src/main/java/nl/vpro/magnolia/jsr107/NodeAwareCacheKeyGenerator.java
@@ -5,8 +5,12 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.io.Serializable;
 import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.cache.annotation.CacheInvocationParameter;
 import javax.cache.annotation.CacheKeyGenerator;
@@ -22,6 +26,33 @@ import javax.jcr.Node;
 @Slf4j
 public class NodeAwareCacheKeyGenerator implements CacheKeyGenerator {
 
+    private static final Map<Class<?>, Method> KNOWNMETHODS = new HashMap<>();
+
+    static {
+        for (String[] classAndMethod : new String[][] {
+            // Let's support some non core magnolia classes to, for which we know a logical serializable key
+            new String[]{"info.magnolia.module.site.Site", "getName"},
+            new String[]{"info.magnolia.dam.api.Item", "getName"},
+        }) {
+            Class<?> c;
+            try {
+                c = Class.forName(classAndMethod[0]);
+            } catch (ClassNotFoundException e) {
+                log.info(e.getMessage());
+                continue;
+            }
+            Method m;
+            try {
+                m = c.getMethod(classAndMethod[1]);
+            } catch (NoSuchMethodException e) {
+                log.error(e.getMessage(), e);
+                continue;
+            }
+            KNOWNMETHODS.put(c, m);
+        }
+
+    }
+
     @Override
     public GeneratedCacheKey generateCacheKey(CacheKeyInvocationContext<? extends Annotation> cacheKeyInvocationContext) {
         List<Serializable> result = new ArrayList<>();
@@ -30,11 +61,22 @@ public class NodeAwareCacheKeyGenerator implements CacheKeyGenerator {
             if (value instanceof Node) {
                 Node node = (Node) value;
                 result.add(NodeUtil.getPathIfPossible(node));
+                continue;
             } else if (value instanceof Serializable) {
                 result.add((Serializable) value);
+                continue;
             } else {
-                throw new IllegalArgumentException("Not serializable " + value);
+                Method m = KNOWNMETHODS.get(value.getClass());
+                if (m != null) {
+                    try {
+                        result.add((String) m.invoke(value));
+                        continue;
+                    } catch (IllegalAccessException | InvocationTargetException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
             }
+            throw new IllegalArgumentException("Not serializable " + value);
         }
 
         return new SerializableGeneratedCacheKey(result.toArray(new Serializable[result.size()]));
